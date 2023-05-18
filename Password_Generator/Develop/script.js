62f2f29ebf2f37dd6f18bd13584198193a3f0608
justmann <mj.justmann@gmail.com>
2023-05-18 18:39:58 -0500

    Updates

--- Password_Generator/Develop/script.js
+++ Password_Generator/Develop/script.js
@@ -1,4 +1,21 @@
 // Assignment code here
+//write the function writePassword
+
+
+//ask for length of a password X 
+     //ask for the prompt. X
+     //save it to variable X
+//check the length prompt to make sure it is between 8 and 128 characters X
+      //if it is not, alert the user to choose a number between 8 and 128 X
+//confirm prompts: 4 fo uppercase, lowercase, numeric, and special characters. X
+    //save it to a variable. X
+    //if the user does not choose at least one of the 4 prompts, alert the user to choose at least one. X
+
+//check to make sure at least one of the 4 prompts is true
+      //combine the confirmed arrays. If not alert user to restart.
+//create an array for uppercase, lowercase, numeric, and special characters
+//create an empty array for the password with length provided by user.
+//random item from combined array and push it to the password array and this step up to the length of the password
 
 
 // Get references to the #generate element
@@ -17,17 +34,12 @@ charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'(
        // for (var i = 0, a = charset.length; i < Number(User); ++i) 
        // {retVal += charset.charAt(Math.floor(Math.random() * a));}}
           
-var characterRequirement = ["!","#","$","%","&'"]
+var characterRequirement = ["!","#","$","%","&'"]  //separated out arrays.
 var numberRequirement = ["0","1","2","3","4","5","6","7","8","9"]
 var lowerLRequirement = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperLRequirement = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var allRequirements = [characterRequirement, numberRequirement, lowerLRequirement, upperLRequirement]
-//var characterNumber = this cannot be right creating all the combitionations outcomes arrays. look in notes. 
-//var characterLower = 
-//var characterUpper =
-//var numberLower = 
-//var numberUpper =
-//var lowerUpper =
+function generatePassword() 
 
 
 
@@ -39,7 +51,7 @@ var allRequirements = [characterRequirement, numberRequirement, lowerLRequiremen
 // Write password to the #password input
 function writePassword() {
   var User = prompt("How many characters would you like your password to contain. Enter a number between 8 and 128.");
-
+   
   if (Number(User) < minRequirement || Number(User) > maxRequirement) {
        alert("Password length must be between 8 and 128 characters. Please try again.");
        writePassword(); } //stops user from continuing and re-sets the generatePassword function.
@@ -123,7 +135,7 @@ function writePassword() {
   
   
       } else {
-    console.log ("Testing")
+   
     
   }
 }
@@ -139,16 +151,3 @@ function writePassword() {
 //((userInput === "r" && compPick === "s") || (userInput === "s" && compPick === "p") || (userInput === "p" && compPick === "r"))
 //this was a faster way, but limited outcomes, but can we apply the same concept for shorter code?
 
-//write the function writePassword
-//ask for length of a password
-     //ask for the prompt. 
-     //save it to variable
-//check the length prompt to make sure it is between 8 and 128 characters
-      //if it is not, alert the user to choose a number between 8 and 128
-//confirm prompts: 4 fo uppercase, lowercase, numeric, and special characters.
-    //save it to a variable.
-//check to make sure at least one of the 4 prompts is true
-      //combine the confired arrays. If not alert user to restart.
-//create an array for uppercase, lowercase, numeric, and special characters
-//create an empty array for the password with length provided by user.
-//random item from combined array and puh it to the password array and this step up to the length of the password
