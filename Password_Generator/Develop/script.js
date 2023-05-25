a118fde6ca8c0e5a11812a23774225ecbb8af2a6
justmann <mj.justmann@gmail.com>
2023-05-24 21:51:40 -0500

    May_24

--- Password_Generator/Develop/script.js
+++ Password_Generator/Develop/script.js
@@ -5,6 +5,8 @@
 //  userInput = prompt("Enter a letter")
 //}
 //alert("Thank you. It was Y")
+
+//Define your arrays
 var charReq = ["!", "#", "$", "%", "&"];
 var numReq = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowReq = [
@@ -64,106 +66,89 @@ var upReq = [
   "Z",
 ];
 //var allReq = [charReq.concat(numReq, lowReq, upReq)]; not needed because it's not user picked. don't need a version for all picked.
-var userPick = [""];
 
+var userPick = [];
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 
 function writePassword() {
-  var passwordLengthReq = undefined; ///this allows us to enter the While Loop.
+  // Prompt the user for password length and validate the input
+  var passwordLengthRaw = prompt(
+    "Please indicate a password length, enter a number between 8 and 128 characters"
+  );
+  var passwordLengthReq = parseInt(passwordLengthRaw);
   //restrict the user with a max and min range. Is the number recevied between 8 and 128?
-  while (
-    //while loop is executing the conditions if they don't meet criteria.
-    passwordLengthReq === undefined || //conditions
-    passwordLengthReq > 128 ||
-    passwordLengthReq < 8 ||
-    passwordLengthReq === NaN
-  ) {
-    //execute this if..
-    if (
-      //if user does this
-      passwordLengthReq > 128 ||
-      passwordLengthReq < 8 ||
-      passwordLengthReq === NaN
-    ) {
-      alert("Number is not in between 8 and 128. Try again"); //then you are going to do this.
-    }
-
-    var passwordLengthRaw = prompt(
-      //this is the variable action to the user.
+  while (passwordLengthReq < 8 || passwordLengthReq > 128) {
+    //while loop is executing the conditions if they don't meet criteria.   //execute this if..
+    alert("Number is not in between 8 and 128. Try again");
+    passwordLengthRaw = prompt(
       "Please indicate a password length, enter a number between 8 and 128 characters"
     );
-    passwordLengthReq = Number(passwordLengthRaw); //converts to number.
-    //Not usable. Ask for new number
+    passwordLengthReq = parseInt(passwordLengthRaw); //parseInt still changes it to number. Received from office hours, originally, was using numbers.
   }
-  //what they gave us is a number in the range
-  var message = confirm(
-    "To create your password, please determine what should be included. One of the four options must be selected. Click ok to continue."
-  );
-  var gotAnyCharTypes = undefined; //allows to enter the while Loop as user has not indicated what they want, so undefined.
-  var specChar = undefined;
-  var numChar = undefined;
-  var numLow = undefined;
-  var numUpper = undefined;
-  //Enters in the While Loop //executes if the user does not define any of the conditions as true.
-  while (gotAnyCharTypes === undefined || gotAnyCharTypes === false) {
-    var specChar = confirm("Click OK to confirm including special characters."); //confirm will return a Boolean
-    var numChar = confirm("Click OK to confirm including numeric characters."); //confirm will return a Boolean
-    var numLow = confirm("Click OK to confirm including lowercase characters."); //confirm will return a Boolean
-    var numUpper = confirm(
-      "Click OK to confirm including uppercase characters."
-    ); //confirm will return a Boolean
 
-    gotAnyCharTypes = specChar || numChar || numLow || numUpper; //if true/true/true/true or even if //true/false/false/false, this will continue to generate password.
+  // Ask the user what types of characters they want to include
+  var specChar = confirm("Click OK to confirm including special characters.");
+  var numChar = confirm("Click OK to confirm including numeric characters.");
+  var numLow = confirm("Click OK to confirm including lowercase characters.");
+  var numUpper = confirm("Click OK to confirm including uppercase characters.");
 
-    if (gotAnyCharTypes === false) {
-      //if all listed as false. or not true
-      //but if ALL are marked as false. This returns to the var message rather than going back all the way to beginning.
-      alert("Did not slect at least one type. Please choose one");
+  //if NOT true AND this is not true AND (not or in this case.
+  if (!specChar && !numChar && !numLow && !numUpper) {
+    alert("Did not select at least one type. Please choose one");
+    writePassword(); //take them back to the beginning
+  }
+
+  //   gotAnyCharTypes = specChar || numChar || numLow || numUpper; //if true/true/true/true or even if //true/false/false/false, this will continue to generate password.
+
+  //   if (gotAnyCharTypes === false) {
+  //     //if all listed as false. or not true
+  //     //but if ALL are marked as false. This returns to the var message rather than going back all the way to beginning.
+  //     alert("Did not slect at least one type. Please choose one");
+  //   }
+  // }
+  //made this too confusing.
+  // var gotAnyCharTypes = undefined; //allows to enter the while Loop as user has not indicated what they want, so undefined.
+  // var specChar = undefined;
+  // var numChar = undefined;
+  // var numLow = undefined;
+  // var numUpper = undefined;
+  // Add the selected character types to the userPick array
+  if (specChar) {
+    userPick = userPick.concat(charReq);
+    console.log(userPick);
     }
-  }
-  //now, what do we know? We know if the user returned at least one true value to in either specChar, numChar, numLow, or numUp.
-  //what does the computer need to do now. So if, specChar is true || muchar is true || low is true || up || true
+    if (numChar) {
+    userPick = userPick.concat(numReq);
+    console.log(userPick);
+    }
+    if (numLow) {
+    userPick = userPick.concat(lowReq);
+    console.log(userPick);
+    }
+    if (numUpper) {
+    userPick = userPick.concat(upReq);
+    console.log(userPick);
+    }
 
-  //if statements will say if a character set returns true to combine it into the userPick arrays.
-  if (specChar === true) {
-    userPick = userPick.concat(specChar);
-  }
-  if (numChar === true) {
-    userPick = userPick.concat(numChar);
-  }
-  if (numLow === true) {
-    userPick = userPick.concat(numLow);
-  }
-  if (numUpper === true) {
-    userPick = userPick.concat(numUpper);
-  }
   //okay. now we should have a user picked array, which will only concat if true. Now we need to create function that creates the generated password.
   //we will need the iterations here based on the number the user entered. so we will be working with
   //the number the user entered as that is a number. so let's write a function to excecute the picks.
   //What does the function need to do? From here it needs
-  
-//returning undefined because of where my variable is listed in the fuction, it still believes that var is undefined.
 
-var userPickLength = Number(passwordLengthReq)
- 
-
+  //returning undefined because of where my variable is listed in the fuction, it still believes that var is undefined.
   function generatePassword() {
-   
-
-    for (indx = 0; indx <= userPickLength.length; i++) {
-      var randomUserPick = userPick[Math.floor(Math.random() * userPickLength.length)];
+    var password = "";
+    for (i = 0; i < passwordLengthReq; i++) {
+    password += userPick[Math.floor(Math.random() * userPick.length)];
     }
-  }
+    return password;
+    }
+    
+
   var password = generatePassword();
-
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
+  }
+  
+  generateBtn.addEventListener("click", writePassword);
--- Password_Generator/Develop/script_20230521.js
+++ Password_Generator/Develop/script_20230521.js
@@ -39,7 +39,7 @@ var numReq = ["0","1","2","3","4","5","6","7","8","9"]
 var lowReq= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upReq= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var allReq = [charReq.concat(numReq, lowReq, upReq)]
-var userPick = [""]
+var userPick = []
 console.log (allReq)
 
 //Do what you know first:
